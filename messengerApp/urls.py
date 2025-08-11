13589979855012b84ff9674455bff26c4107a2ca
Vladislav123-hue <6o4tqdmx@students.codeinstitute.net>
2025-08-11 12:45:12 +0300

    custom register form

--- messengerApp/urls.py
+++ messengerApp/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', views.Login, name="LoginPage")
+    path('login/', views.Login, name="LoginPage"),
+    path('register/', views.Register, name="RegisterPage")
 ]
--- messengerApp/views.py
+++ messengerApp/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def Login(request):
      return render (request, 'Login.html')
+
+def Register(request):
+     return render (request, 'Register.html')
