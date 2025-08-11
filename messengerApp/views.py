07c39b6d11f1ca4e4012ecff852c776b3af22963
Vladislav123-hue <6o4tqdmx@students.codeinstitute.net>
2025-08-11 16:04:29 +0300

    implemet authorization and registration, create superuser

--- messengerApp/views.py
+++ messengerApp/views.py
@@ -1,9 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def Login(request):
+     if request.method == "POST":
+          username = request.POST.get('Username')
+          password = request.POST.get('Password')
+
+          user = authenticate(request, username=username, password=password)
+
+          if user is not None:
+               login(request, user)
+               return redirect('home')
+          else:
+               error = "False login or password" 
+               return render (request, 'Login.html', {'error' : error})
+
      return render (request, 'Login.html')
 
 def Register(request):
-     return render (request, 'Register.html')
+    if request.method == "POST":
+        print(request.POST)
+        username = request.POST.get('Username')
+        email = request.POST.get('Email')
+        password1 = request.POST.get('Password1')
+        password2 = request.POST.get('Password2')
+        first_name = request.POST.get('First_name')
+        last_name = request.POST.get('Last_name')
+
+        if password1 != password2:
+            error = "Passwords not equal"
+            return render(request, 'register.html', {'error': error})
+
+        # Проверка, что пользователь с таким именем ещё не существует
+        if User.objects.filter(username=username).exists():
+            error = "Такой логин уже есть"
+            return render(request, 'register.html', {'error': error})
+
+        # Создаём пользователя
+        User.objects.create_user(username=username, email=email, password=password1, first_name=first_name, last_name=last_name)
+        return redirect('LoginPage')  # После регистрации — на логин
+
+    return render(request, 'register.html')
+     
