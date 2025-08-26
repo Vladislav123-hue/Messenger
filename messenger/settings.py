7ad1a303b1cbdd7d0086c40bf446b04c61cd9624
Vladislav123-hue <6o4tqdmx@students.codeinstitute.net>
2025-08-26 15:43:37 +0300

    install gunicorn, add procfile, configure settings.py

--- messenger/settings.py
+++ messenger/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xsnv)w2q963rq3els(_+veb@=1tpcv65pj0b%(snm^s*))t!p_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
